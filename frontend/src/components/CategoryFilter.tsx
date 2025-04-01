ff8fd096781ff33cf16d4d008223243a4aa3d31c
Jonathan Beales <jonathansbeales@gmail.com>
2025-04-01 10:13:19 -0600

    Fixed Something

--- frontend/src/components/CategoryFilter.tsx
+++ frontend/src/components/CategoryFilter.tsx
@@ -15,7 +15,7 @@ function CategoryFilter ({
     useEffect(() => {
         const fetchCategories = async () => {
             try {
-            const response = await fetch("https://localhost:5000/Water/GetProjectTypes");
+            const response = await fetch("https://waterproject-beales-backend.azurewebsites.net/WaterGetProjectTypes");
             const data = await response.json();
             console.log('Fetched categories:', data)
             setCategories(data);
